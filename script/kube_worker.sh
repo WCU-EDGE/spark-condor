e4e581d3f77f802d04f3e6ea432863b6f428d63a
Linh B Ngo <lngo@wcupa.edu>
2020-10-18 17:19:34 -0400

    Update kube_worker.sh

--- script/kube_worker.sh
+++ script/kube_worker.sh
@@ -12,5 +12,6 @@ while [ ! -f /opt/keys/kube.log ]; do
   sleep 20
 done
 
-command=`tail -n 2 /opt/keys/kube.log | tr -d '\\'`
+#command=`tail -n 2 /opt/keys/kube.log | tr -d '\\'`
+command=`tail -n 2 /opt/keys/kube.log`
 sudo $command
