4ee51b497bf1c575b92855831926f4f62433b994
Nick Edwards <nicholas6598@gmail.com>
2020-10-20 18:33:32 -0400

    added new docker

--- script/install_docker.sh
+++ script/install_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -x
+#set -x
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux sudo apt gnupg2 pass
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo apt-get install -y httping
-sudo apt-get install -y jq
+#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux sudo apt gnupg2 pass
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt-get update
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+#sudo apt-get install -y httping
+#sudo apt-get install -y jq
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
 # the username needs to be changed
 while IFS= read -r line; do
