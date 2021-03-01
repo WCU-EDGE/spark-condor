968b04f06888a4277469580a2dd708b322efaa07
Nick Edwards <nicholas6598@gmail.com>
2021-03-01 14:29:32 -0500

    fixed typos

--- profile.py
+++ profile.py
@@ -6,7 +6,7 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 tourDescription = \
-"""
+    """
 This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
 """
 
@@ -14,7 +14,7 @@ This profile provides the template for a compute node with Docker installed on U
 # Setup the Tour info with the above description and instructions.
 #  
 tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Description(IG.Tour.TEXT, tourDescription)
 request.addTour(tour)
 
 prefixForIP = "192.168.1."
@@ -22,38 +22,40 @@ link = request.LAN("lan")
 
 num_nodes = 3
 for i in range(num_nodes):
-  if i == 0:
-    node = request.XenVM("head")
-  elif i == 1:
-    node = request.XenVM("submit_machine")
-  else:
-    node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
-  node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
-  # setup Docker
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_docker.sh"))
-  # setup Kubernetes
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_kubernetes.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
-  # setup Condor
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_condor.sh))
-  if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/central_manager.sh"))
-  elif i == 1:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/submit_machine.sh"))
-  else:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/execute_machine.sh"))
-  
-  if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/kube_manager.sh"))
-  else:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/kube_worker.sh"))
-      
+    if i == 0:
+        node = request.XenVM("head")
+    elif i == 1:
+        node = request.XenVM("submit_machine")
+    else:
+        node = request.XenVM("worker-" + str(i))
+    node.cores = 4
+    node.ram = 8192
+    node.routable_control_ip = "true"
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+    iface = node.addInterface("if" + str(i))
+    iface.component_id = "eth1"
+    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+    link.addInterface(iface)
+
+    # setup Docker
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_docker.sh"))
+    # setup Kubernetes
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_kubernetes.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+
+    # setup Condor
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/install_condor.sh"))
+    if i == 0:
+        node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/central_manager.sh"))
+    elif i == 1:
+        node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/submit_machine.sh"))
+    else:
+        node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/execute_machine.sh"))
+
+    # setup Kubernetes
+    if i == 0:
+        node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/kube_manager.sh"))
+    else:
+        node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/script/kube_worker.sh"))
+
 pc.printRequestRSpec(request)
